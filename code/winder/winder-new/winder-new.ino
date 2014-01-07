671ea92630af4871eaf89e0031438cbc6cd825e8
Albert La <albert.la@usask.ca>
2014-01-07 14:26:33 -0600

    New winder Code
    January 7, 2014 - Trunk (Albert La)
    -added winder-new
    -restructured variables and constants
    -added menu on startup
    -added serial output of data

---
+++ code/winder/winder-new/winder-new.ino
@@ -0,0 +1,82 @@
+/************************************/
+/*	Arduino Code for the Winder		*/
+/*	Written By Albert La			*/
+/*	January 7, 2014					*/
+/************************************/
+
+//Pin Assignments
+#define mot1Step	2
+#define mot1Dir		3
+#define mot2Step	4
+#define mot2Dir		5
+#define motEnable	10
+
+//Variables
+int speedDividerCount = 0;
+int loopCount = 0;
+int globalSpeed;
+int coilMotStepCount = 0; 
+int coilTurns = 0;
+int spoolCount = 0; 
+int spoolDetect = 0;
+int spoolSensor = 0;
+int isFirstLoops = 1;
+
+double ohmsGoal;
+double ohmsPerMeter;
+double dynamicCoilOhms = 0;
+
+//Constants
+const int COILSPEEDMULTIPLIER = 5;		//speed of the coil motor
+const int SPEEDDIVIDER = 1;  			//speed differnce of the motors
+const int LOOPDIVIDER = 100; 			//We can accomplish N things when we go through the loop
+const int MAXSPEED = 100;				//Maximum speed of the spool
+const int STEPSPERREV = 3200;
+const double SPOOLSENSORCIRC = 250; 	// each time the spool rotates around it is this much wire in mm
+
+// Ohms of each gauge of wire per meter 
+const double AWG42OHMS = 6.07;
+const double AWG40OHMS = 3.42;
+const double AWG38OHMS = 2.19;
+const double AWG36OHMS = 1.3;
+const double AWG34OHMS = .844;
+
+double turnsPerMeter = 1000 / SPOOLSENSORCIRC;
+double ohmsPerSensorTurn =   ohmsPerMeter / turnsPerMeter;
+
+
+// note A0 will control global speed 
+// and A1 will control differnce in speed
+
+// This setup routine runs whenever you reset the Arduino
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  
+  //assign pinmodes to motor control
+  pinMode(mot1Step, OUTPUT);
+  pinMode(mot1Dir, OUTPUT);
+  pinMode(mot2Step, OUTPUT);
+  pinMode(mot2Dir, OUTPUT);
+  pinMode(motEnabel, OUTPUT);
+  
+  //Display menu choose options
+  Serial.println("****************************************");
+  Serial.println("		Choose an option:");
+  Serial.println("	1.  Enter impedance");
+  Serial.println("	2.  Enter wire gauge");
+  Serial.println("	3.  Make coil");
+  Serial.println("****************************************");
+  
+  if (Serial.available() > 0)
+  {
+	int incomingByte = 0;
+	incomingByte = Serial.read();
+	
+	Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+  }
+}
+
+void loop()
+{}
